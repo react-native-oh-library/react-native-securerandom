9fc1c0419a7d9a377e66a9971785e15c5ddd82e6
huangzongzheng <1518931313@qq.com>
2024-07-23 17:24:34 +0800

    fix:codegen报错

--- src/NativeSecureRandom.ts
+++ src/NativeSecureRandom.ts
@@ -1,13 +1,10 @@
 import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
 import { TurboModuleRegistry } from 'react-native';
-interface DataBlob {
-  data: Uint8Array;
+export interface Spec extends TurboModule {
+  readonly randomBytes: (len: number, cb: (err: string, base64String: string) => void) => void
 }
 export interface Spec extends TurboModule {
-  readonly randomBytes: (len: number, cb: (err: string, base64String: string)=> void) => void
-} 
-export interface Spec extends TurboModule {
-  generateSecureRandomAsBase64: (length:number) => Promise<DataBlob>;
+  generateSecureRandomAsBase64: (length: number) => Promise<any>;
 }
 
 export const NativeSecureRandom = TurboModuleRegistry.getEnforcing<Spec>('SecureRandomNativeModule')
