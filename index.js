85f8494af4f53d5abe56db00d05ed4bd15ad0617
huangzongzheng <1518931313@qq.com>
2024-07-24 17:11:21 +0800

    fix:生成codegen报错和导出报错

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native';
-import { generateSecureRandom as generateSecureRandomisIosOrAndroid  } from 'react-native-securerandom';
+import { generateSecureRandom as generateSecureRandomisIosOrAndroid } from 'react-native-securerandom';
 import { harmonyGenerateSecureRandom } from './index.harmony'
 const isIosOrAndroid = Platform.OS === 'android' || Platform.OS === 'ios'
 export const generateSecureRandom = isIosOrAndroid ? generateSecureRandomisIosOrAndroid : harmonyGenerateSecureRandom;
--- src/NativeSecureRandom.ts
+++ src/NativeSecureRandom.ts
@@ -1,13 +1,44 @@
 import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
 import { TurboModuleRegistry } from 'react-native';
+interface Uint8Array {
+  readonly BYTES_PER_ELEMENT: number;
+  readonly buffer: ArrayBufferLike;
+  readonly byteLength: number;
+  readonly byteOffset: number;
+  copyWithin(target: number, start: number, end?: number): this;
+  every(predicate: (value: number, index: number, array: Uint8Array) => unknown, thisArg?: any): boolean;
+  fill(value: number, start?: number, end?: number): this;
+  filter(predicate: (value: number, index: number, array: Uint8Array) => any, thisArg?: any): Uint8Array;
+  find(predicate: (value: number, index: number, obj: Uint8Array) => boolean, thisArg?: any): number | undefined;
+  findIndex(predicate: (value: number, index: number, obj: Uint8Array) => boolean, thisArg?: any): number;
+  forEach(callbackfn: (value: number, index: number, array: Uint8Array) => void, thisArg?: any): void;
+  indexOf(searchElement: number, fromIndex?: number): number;
+  join(separator?: string): string;
+  lastIndexOf(searchElement: number, fromIndex?: number): number;
+  readonly length: number;
+  map(callbackfn: (value: number, index: number, array: Uint8Array) => number, thisArg?: any): Uint8Array;
+  reduce(callbackfn: (previousValue: number, currentValue: number, currentIndex: number, array: Uint8Array) => number): number;
+  reduce(callbackfn: (previousValue: number, currentValue: number, currentIndex: number, array: Uint8Array) => number, initialValue: number): number;
+  reduce<U>(callbackfn: (previousValue: U, currentValue: number, currentIndex: number, array: Uint8Array) => U, initialValue: U): U;
+  reduceRight(callbackfn: (previousValue: number, currentValue: number, currentIndex: number, array: Uint8Array) => number): number;
+  reduceRight(callbackfn: (previousValue: number, currentValue: number, currentIndex: number, array: Uint8Array) => number, initialValue: number): number;
+  reduceRight<U>(callbackfn: (previousValue: U, currentValue: number, currentIndex: number, array: Uint8Array) => U, initialValue: U): U;
+  reverse(): Uint8Array;
+  set(array: ArrayLike<number>, offset?: number): void;
+  slice(start?: number, end?: number): Uint8Array;
+  some(predicate: (value: number, index: number, array: Uint8Array) => unknown, thisArg?: any): boolean;
+  sort(compareFn?: (a: number, b: number) => number): this;
+  subarray(begin?: number, end?: number): Uint8Array;
+  toLocaleString(): string;
+  toString(): string;
+  valueOf(): Uint8Array;
+  [index: number]: number;
+}
 interface DataBlob {
   data: Uint8Array;
 }
 export interface Spec extends TurboModule {
-  readonly randomBytes: (len: number, cb: (err: string, base64String: string)=> void) => void
-} 
-export interface Spec extends TurboModule {
-  generateSecureRandomAsBase64: (length:number) => Promise<DataBlob>;
+  generateSecureRandomAsBase64: (length: number) => Promise<DataBlob>;
 }
 
 export const NativeSecureRandom = TurboModuleRegistry.getEnforcing<Spec>('SecureRandomNativeModule')
