6830d2314a54c7d29555b7ed2edbf96b589d91f6
huangzongzheng <1518931313@qq.com>
2024-12-18 11:37:51 +0800

    chore: 五项核查整改

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { Platform } from 'react-native';
 import { generateSecureRandom as generateSecureRandomisIosOrAndroid } from 'react-native-securerandom';
 import { harmonyGenerateSecureRandom } from './index.harmony'
